e90e544900561e0be8218ed554396fe4f9a3e3c9
Leander.Nachtmann <Leander.nachtmann@proximity.de>
2019-04-09 12:22:57 +0200

    added Exercise 5 - 7

--- blatt0.py
+++ blatt0.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def is_palindrome(word):
     i = 0
     for s in reversed(word):
@@ -18,6 +21,8 @@ def print_pascal(n):
 
 
 def flatten(not_flat):
+    if not isinstance(not_flat, list):
+        raise Exception()
     result = []
     for l in not_flat:
         if isinstance(l, list):
@@ -33,7 +38,53 @@ def flatten(not_flat):
     return result
 
 
+def solve_equation(a, b, c):
+    p = b / a
+    q = c / a
+    x1 = -(p / 2) + sqrt((p / 2) ** 2 - q)
+    x2 = -(p / 2) - sqrt((p / 2) ** 2 - q)
+    return x1, x2
+
+
+def fizz_buzz(n):
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("Fizz Buzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(i)
+        print(result[-1])
+    return result
+
+
+def mybin(x):
+    result = ""
+    a = x
+    while a != 0:
+        x = a
+        a = int(x / 2)
+        b = x % 2
+        result = str(b) + result
+    return "0b" + result
+
+
+def myint(x):
+    x = x[2:]
+    result = 0
+    count = 0
+    for i in reversed(x):
+        if i == "1":
+            result += 2 ** count
+        print(result)
+        count += 1
+    return result
+
+
 # print(is_palindrome("test"))
 # print(is_palindrome("aaabbbaaa"))
 # print_pascal([1, '\n', 1, 1, '\n', 1, 2, 1, '\n'])
-print(flatten([1, 2, 3, [1, 2, 3], [4, 5, [6, 7, [8, 9]]]]))
+# print(flatten([1, 2, 3, [1, 2, 3], [4, 5, [6, 7, [8, 9]]]]))
--- test_blatt0.py
+++ test_blatt0.py
@@ -1,9 +1,6 @@
-from blatt0 import flatten
+import random
 
-
-# from blatt0 import fizz_buzz
-# from blatt0 import solve_equation
-# from blatt0 import myint, mybin
+from blatt0 import flatten, solve_equation, fizz_buzz, myint, mybin
 
 
 def test_pascal():
@@ -19,22 +16,41 @@ def test_flatten_recursiv():
     assert flatten([1, 2, 3, [1, 2, 3], [4, 5, [6, 7, [8, 9]]]]) == [1, 2, 3, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-def test_fizz_buzz():
-    pass  # TODO
+# @pytest.mark.xfail(raises=Exception)
+# def test_flatten_error():
+#     flatten(1)
 
 
 def test_solve_equation():
-    pass  # TODO
+    x1, x2 = solve_equation(1, 4, 4)
+    assert_equation(x1, x2, -2)
 
-# def test_int_to_bin():
-#     input = random.sample(range(1000), 10)
-#     for x in input:
-#         assert mybin(x) == bin(x)
-#
-#
-# def test_bin_to_int():
-#     input = [(i, bin(i)) for i in random.sample(range(1000), 10)]
-#     for i, bini in input:
-#         assert myint(bini) == i
 
-# TODO: more tests
+def test_solve_equation_2():
+    x1, x2 = solve_equation(1, -3, -4)
+    assert_equation(x1, x2, 4)
+    assert_equation(x1, x2, -1)
+
+
+def assert_equation(x1, x2, expected):
+    assert x1 == expected or x2 == expected
+
+
+def test_fizz_buzz():
+    assert fizz_buzz(5) == [1, 2, 'Fizz', 4, 'Buzz']
+
+
+def test_fizz_buzz_high():
+    assert fizz_buzz(500)[-1] == 'Buzz'
+
+
+def test_int_to_bin():
+    input = random.sample(range(1000), 10)
+    for x in input:
+        assert mybin(x) == bin(x)
+
+
+def test_bin_to_int():
+    input = [(i, bin(i)) for i in random.sample(range(1000), 10)]
+    for i, bini in input:
+        assert myint(bini) == i
